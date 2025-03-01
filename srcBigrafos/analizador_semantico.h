dd361e67de3c1763a05f40497de99807e7bed260
Daniel Villamor <dvillamor@unal.edu.co>
2025-03-01 11:12:13 -0500

    se borran los generados en bigrafos

--- srcBigrafos/analizador_semantico.h
+++
@@ -1,8 +0,0 @@
-#ifndef ANALIZADOR_SEMANTICO_H
-#define ANALIZADOR_SEMANTICO_H
-
-#include "ast.h"
-
-void init_semantic_analysis(Node *ast);
-
-#endif /* ANALIZADOR_SEMANTICO_H */
--- srcBigrafos/intermediate_code.h
+++
@@ -1,8 +0,0 @@
-#ifndef INTERMEDIATE_CODE_H
-#define INTERMEDIATE_CODE_H
-
-#include "ast.h"
-
-void generate_intermediate_code(Node *ast, const char *output_file);
-
-#endif /* INTERMEDIATE_CODE_H */
